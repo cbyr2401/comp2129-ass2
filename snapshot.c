62c677fbe323e12988695c3930c404b340e7a5a0
Cian Byrne <Cian Byrne>
2016-04-08 12:53:47 +1000

    initial commit

---
+++ snapshot.c
@@ -0,0 +1,67 @@
+/**
+ * comp2129 - assignment 2
+ * <your name>
+ * <your unikey>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+
+#include "snapshot.h"
+
+entry* entry_head = NULL;
+entry* entry_tail = NULL;
+
+snapshot* snapshot_head = NULL;
+snapshot* snapshot_tail = NULL;
+
+//
+// We recommend that you design your program to be
+// modular where each function performs a small task
+//
+// e.g.
+//
+// command_bye
+// command_help
+// ...
+// entry_add
+// entry_delete
+// ...
+//
+
+void command_bye () {
+    printf("bye\n");
+}
+
+void command_help() {
+    printf("%s\n", HELP);
+}
+
+int main(void) {
+
+	char line[MAX_LINE];
+
+	while (true) {
+    	printf("> ");
+		// get input from user and check it is valid
+		//input = fgets(buffer, 12, stdin);
+		// check for null
+		//if(input=='\0') CleanExitError();
+		// parse
+		//nargs = sscanf(input, " %s %c", &cmd, &x, &y, extra);
+
+    	if (fgets(line, MAX_LINE, stdin) == NULL) {
+    	    command_bye();
+    	    return 0; 		
+    	}
+    	
+	    //
+	    // TODO
+	    //
+  	}
+
+	return 0;
+}
